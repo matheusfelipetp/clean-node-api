81bab2d7541e8aee2a083db157394e47465ea8a2
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-11 15:51:33 -0300

    chore: change eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import love from "eslint-config-love";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**", "coverage/**"],
+  },
+  {
+    ...love,
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      ...(love.languageOptions ?? {}),
+      globals: {
+        ...(love.languageOptions?.globals ?? {}),
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "class-methods-use-this": "off",
+      "no-magic-numbers": "off",
+    },
+  },
+];
