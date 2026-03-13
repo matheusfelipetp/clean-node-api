44455085cca9d911b6c6b0cac812e96877a485cd
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-13 10:06:10 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/useCases/add-account/db-add-account.spec.ts
+++ src/data/useCases/add-account/db-add-account.spec.ts
@@ -99,4 +99,20 @@ describe("DbAddAccount Usecase", () => {
       password: "hashed_password",
     });
   });
+
+  test("Should throw if AddAccountRepository throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(new Promise((_, reject) => reject(new Error())));
+
+    const data = {
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "invalid_password",
+    };
+
+    const promise = sut.add(data);
+    await expect(promise).rejects.toThrow();
+  });
 });
