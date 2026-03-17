26285004e3e03357fbff5880291beafcf6317353
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-17 14:38:07 -0300

    feat: connect to mongodb before app listen

--- src/main/routes/signup/signup-routes.test.ts
+++ src/main/routes/signup/signup-routes.test.ts
@@ -1,10 +1,11 @@
 import request from "supertest";
 import { MongoHelper } from "../../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../../config/app";
+import env from "../../config/env";
 
 describe("SignUp Routes", () => {
   beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL as string);
+    await MongoHelper.connect(env.mongoUrl);
   });
 
   afterAll(async () => {
