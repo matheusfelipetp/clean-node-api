df5fbe58f82a8ce2cdd026c6c4939f9ca336931a
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-18 10:24:12 -0300

    feat: ensure LogMongoRepository create an error log on success

--- src/main/decorators/log/log.spec.ts
+++ src/main/decorators/log/log.spec.ts
@@ -38,7 +38,7 @@ const makeFakeServerError = (): HttpResponse => {
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log(stack: string): Promise<void> {
+    async logError(stack: string): Promise<void> {
       return new Promise((resolve) => resolve());
     }
   }
@@ -86,7 +86,7 @@ describe("LogController Decorator", () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
 
     const error = makeFakeServerError();
-    const logSpy = jest.spyOn(logErrorRepositoryStub, "log");
+    const logSpy = jest.spyOn(logErrorRepositoryStub, "logError");
 
     jest
       .spyOn(controllerStub, "handle")
