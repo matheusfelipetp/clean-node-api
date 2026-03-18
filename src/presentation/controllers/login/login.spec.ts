a3513da2aabdadd7c407496793ae5691817ae8bf
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-18 15:00:11 -0300

    test: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,4 +1,8 @@
-import { InvalidParamError, MissingParamError } from "../../errors";
+import {
+  InvalidParamError,
+  MissingParamError,
+  ServerError,
+} from "../../errors";
 import {
   badRequest,
   serverError,
@@ -94,7 +98,7 @@ describe("Login Controller", () => {
       throw new Error();
     });
     const httpResponse = await sut.handle(makeFakeRequest());
-    expect(httpResponse).toEqual(serverError(new Error()));
+    expect(httpResponse).toEqual(serverError(new ServerError()));
   });
 
   test("Should call Authentication with correct values", async () => {
@@ -116,5 +120,14 @@ describe("Login Controller", () => {
 
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(unauthorized());
+  });
+
+  test("Should return 500 if Authentication throws", async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest.spyOn(authenticationStub, "auth").mockImplementationOnce(() => {
+      return new Promise((_, reject) => reject(new Error()));
+    });
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new ServerError()));
   });
 });
