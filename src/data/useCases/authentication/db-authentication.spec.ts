181585a9f151b177fae5b2fdc78b8eec84c57e50
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-19 13:58:14 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/useCases/authentication/db-authentication.spec.ts
@@ -0,0 +1,50 @@
+import { AuthenticationModel } from "../../../domain/useCases/authentication";
+import { LoadAccountByEmailRepository } from "../../protocols/load-account-by-email-repository";
+import { AccountModel } from "../add-account/db-add-account-protocols";
+import { DbAuthentication } from "./db-authentication";
+
+interface SutTypes {
+  sut: DbAuthentication;
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: "any_id",
+  name: "any_name",
+  email: "any_email@mail.com",
+  password: "any_password",
+});
+
+const makeFakeAuthentication = (): AuthenticationModel => ({
+  email: "any_email@mail.com",
+  password: "any_password",
+});
+
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async load(email: string): Promise<AccountModel> {
+      return new Promise((resolve) => resolve(makeFakeAccount()));
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository();
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+  return { sut, loadAccountByEmailRepositoryStub };
+};
+
+describe("DBAuthentication UseCase", () => {
+  test("Should call LoadAccountByEmailRepository with correct email", async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, "load");
+
+    await sut.auth({
+      email: "any_email@mail.com",
+      password: "any_password",
+    });
+
+    expect(loadSpy).toHaveBeenCalledWith("any_email@mail.com");
+  });
+});
