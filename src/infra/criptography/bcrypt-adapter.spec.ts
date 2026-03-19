588e4b3d938019a3dc4398b9c993ea933166ade0
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-19 17:07:33 -0300

    feat: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -5,6 +5,9 @@ jest.mock("bcrypt", () => ({
   async hash(): Promise<string> {
     return new Promise((resolve) => resolve("hash"));
   },
+  async compare(): Promise<boolean> {
+    return new Promise((resolve) => resolve(true));
+  },
 }));
 
 const salt = 12;
@@ -14,7 +17,7 @@ const makeSut = (): BcryptAdapter => {
 };
 
 describe("Bcrypt Adapter", () => {
-  test("Should call bcrypt with correct values", async () => {
+  test("Should call hash with correct values", async () => {
     const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, "hash");
 
@@ -22,13 +25,13 @@ describe("Bcrypt Adapter", () => {
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
   });
 
-  test("Should return a hash on success", async () => {
+  test("Should return a valid hash on hash success", async () => {
     const sut = makeSut();
     const hash = await sut.hash("any_value");
     expect(hash).toBe("hash");
   });
 
-  test("Should throw if bcrypt throws", async () => {
+  test("Should throw if hash throws", async () => {
     const sut = makeSut();
     jest.spyOn(bcrypt, "hash").mockImplementationOnce(() => {
       throw new Error();
@@ -36,5 +39,18 @@ describe("Bcrypt Adapter", () => {
 
     const promise = sut.hash("any_value");
     await expect(promise).rejects.toThrow();
+  });
+
+  test("Should call compare with correct values", async () => {
+    const sut = makeSut();
+    const compareSpy = jest.spyOn(bcrypt, "compare");
+    await sut.compare("any_value", "any_hash");
+    expect(compareSpy).toHaveBeenCalledWith("any_value", "any_hash");
+  });
+
+  test("Should return true when compare succeeds", async () => {
+    const sut = makeSut();
+    const hash = await sut.hash("any_value");
+    expect(hash).toBe("hash");
   });
 });
