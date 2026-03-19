e0e89703bf9e7872c0418584323c7c81b9b59aa9
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-19 17:12:48 -0300

    test: add new tests in BcryptAdapter

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -50,7 +50,17 @@ describe("Bcrypt Adapter", () => {
 
   test("Should return true when compare succeeds", async () => {
     const sut = makeSut();
-    const hash = await sut.hash("any_value");
-    expect(hash).toBe("hash");
+    const isValid = await sut.compare("any_value", "any_hash");
+    expect(isValid).toBe(true);
+  });
+
+  test("Should return false when compare fails", async () => {
+    const sut = makeSut();
+    jest.spyOn(bcrypt, "compare").mockImplementationOnce(() => {
+      return new Promise((resolve) => resolve(false));
+    });
+
+    const isValid = await sut.compare("any_value", "any_hash");
+    expect(isValid).toBe(false);
   });
 });
