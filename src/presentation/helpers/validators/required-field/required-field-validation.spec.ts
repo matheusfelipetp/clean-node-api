94b96c9f9f8efb133bd92316209e6b36becc599a
Matheus Felipe Teixeira Pinho <matheus.teixeira@senac.br>
2026-03-19 10:03:57 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
@@ -2,19 +2,19 @@ import { MissingParamError } from "../../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
 const makeSut = (): RequiredFieldValidation => {
-  return new RequiredFieldValidation("any_field");
+  return new RequiredFieldValidation("field");
 };
 
 describe("RequiredField Validation", () => {
   test("Should return a MissingParamError if validation fails", () => {
     const sut = makeSut();
-    const error = sut.validate({ name: "any_name" });
-    expect(error).toEqual(new MissingParamError("any_field"));
+    const error = sut.validate({ name: "name" });
+    expect(error).toEqual(new MissingParamError("field"));
   });
 
   test("Should not return if validation succeeds", () => {
     const sut = makeSut();
-    const error = sut.validate({ any_field: "any_value" });
+    const error = sut.validate({ field: "value" });
     expect(error).toBeFalsy();
   });
 });
